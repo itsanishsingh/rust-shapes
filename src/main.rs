f15be7cc2ce0a596344821011afa08e77e9e560b
itsanishsingh <itzanishsingh@gmail.com>
2024-11-06 10:00:42 +0530

    Added square with a commented operations trait

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ struct Rectangle {
     width: f64,
 }
 
-trait ShapeOperations {
-    fn new(length: f64, width: f64) -> Self;
-    fn area(&self) -> f64;
-    fn perimeter(&self) -> f64;
+struct Square {
+    size: f64,
 }
 
-impl ShapeOperations for Rectangle {
+// trait ShapeOperations {
+//     fn new() -> Self;
+//     fn area(&self) -> f64;
+//     fn perimeter(&self) -> f64;
+// }
+
+impl Rectangle {
     fn new(length: f64, width: f64) -> Rectangle {
         Rectangle { length, width }
     }
@@ -23,10 +27,32 @@ impl ShapeOperations for Rectangle {
     }
 }
 
+impl Square {
+    fn new(size: f64) -> Square {
+        Square {size}
+    }
+
+    fn area(&self) -> f64 {
+        return self.size*self.size;
+    }
+
+    fn perimeter(&self) -> f64 {
+        return self.size*4 as f64;
+    }
+}
+
 fn main() {
     let length: f64 = 5 as f64;
     let width: f64 = 3 as f64;
+
+    let size: f64 = 4 as f64;
+
     let rect = Rectangle::new(length, width);
-    println!("The perimeter is {}", rect.perimeter());
-    println!("The area is {}", rect.area());
+    let sq = Square::new(size);
+
+    println!("The perimeter of rectangle is {}", rect.perimeter());
+    println!("The area of rectangle is {}", rect.area());
+
+    println!("The perimeter of square is {}", sq.perimeter());
+    println!("The area of square is {}", sq.area());
 }
